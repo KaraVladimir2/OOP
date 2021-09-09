6c11663e9a15b72109b856f3adc40cdc559c1199
KaraVladimir2 <vova.kara1996@gmail.com>
2021-09-09 22:05:07 +0300

    1

--- LAB 10Д, 10Е/LAB 10E/Header 10Е.h
+++ LAB 10Д, 10Е/LAB 10E/Header 10Е.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+class mas{
+	int n, m;
+	int** arr;
+public:
+	mas() { n = m = 1; arr = new int* [n]; for (int i = 0; i < 1; i++) { arr[i] = new int[m]; }; }
+	mas(int n_size, int m_size) { n = n_size; m = m_size; arr = new int* [n]; for (int i = 0; i < n; i++) { arr[i] = new int[m]; }; }
+	void find_min(int x,int y);
+	void gen();
+	void show();
+	void find_and_change();
+	void podarr();
+};
---
+++ LAB 10Д, 10Е/LAB 10E/Source 10Е.cpp
@@ -0,0 +1,104 @@
+#include "Header 10Е.h"
+
+void mas::podarr() {
+    int stroka, stolb, x, y;
+    cout << "Введите размер подматрицы(строка, столбец): ";
+    cin >> stroka >> stolb;
+    cout << "Введите начальную позицию подматрицы(строка, столбец): ";
+    cin >> x >> y;
+    for (int i = y-1; i < stolb; i++) {
+        for (int j = x-1; j < stroka; j++) {
+            cout << arr[i][j] << "\t";
+        }
+        cout << endl;
+    }           
+}
+
+void mas::find_and_change() {
+    int x, y, q;
+    string k;
+    cout << "Введите координаты элемента(строка, столбец): ";
+    cin >> x >> y;
+    cout << arr[x-1][y-1] << endl;
+    cout << "Желаете заменить элемент?(y,n)";
+    cin >> k;
+    if (k == "y") {
+        cout << "Введите новое значение: ";
+        cin >> q;
+        arr[x-1][y-1] = q;
+    }
+}
+
+void mas::find_min(int x,int y) {
+    int min = arr[0][0];
+    int index1(0), index2(0);
+    for (int i = 0; i < y; i++)
+        for (int j = 1; j < x; j++)
+            if (min > arr[i][j])
+            {
+                min = arr[i][j];
+                index1 = i;
+                index2 = j;
+            }
+    cout << index1+1<<" строка" << " " << index2 + 1<<" столбец" << endl;
+}
+
+void mas::gen()
+{
+    srand(time(NULL));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            arr[i][j] = rand() % 100;
+        }
+    }
+}
+
+void mas::show()
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+
+int main(){
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int k(10), x, y;
+    cout << "Введите размер массива(строка, столбец): ";
+        cin >> x >> y;
+    mas ob(x,y);
+    
+    while (k>0)
+    {
+        cout << "Введите соответствующую цифру(1-генерация случайного массива, 2-показать массив,\n3-найти координаты минимального жлемента, 4-найти и заменить, 5-выделить подматрицу): ";
+        cin >> k;
+        if (k == 1) {
+            k = 10;
+            ob.gen();
+        }
+        else if (k == 2) {
+            ob.show();
+            k = 10;
+        }
+        else if (k == 3) {
+            ob.find_min(x,y);
+            k = 10;
+        }
+        else if (k == 4) {
+            ob.find_and_change();
+        }
+        else if (k == 5) {
+            ob.podarr();
+        }
+
+    }
+
+}
