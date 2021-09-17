243eeb3e48c1bb88b22633b781817272a8e883bd
KaraVladimir2 <vova.kara1996@gmail.com>
2021-09-17 09:30:24 +0300

    10Е

--- LAB 10Д, 10Е/LAB 10E/Header 10Е.h
+++ LAB 10Д, 10Е/LAB 10E/Header 10Е.h
@@ -7,7 +7,6 @@ class mas{
 	int n, m;
 	int** arr;
 public:
-	mas() { n = m = 1; arr = new int* [n]; for (int i = 0; i < 1; i++) { arr[i] = new int[m]; }; }
 	mas(int n_size, int m_size) { n = n_size; m = m_size; arr = new int* [n]; for (int i = 0; i < n; i++) { arr[i] = new int[m]; }; }
 	void find_min(int x,int y);
 	void gen();
