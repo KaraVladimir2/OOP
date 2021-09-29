a24d0b66f20fb8fc0324d73d785a333677c54d05
KaraVladimir2 <vova.kara1996@gmail.com>
2021-09-29 21:15:43 +0300

    2

---
+++ LAB 2/LAB 2/Header.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class PEVM {
+	string proc;
+	int mgz;
+	int ram;
+	string type;
+public:
+	void set_proc(string p);
+	void set_mgz(int m);
+	void set_ram(int r);
+	void set_type(string t);
+
+	string get_proc() const;
+	int get_mgz() const;
+	int get_ram() const;
+	string get_type() const;
+
+	void shapka();
+
+	PEVM operator = (const PEVM& ob);
+	friend PEVM operator + (const PEVM& ob1, const PEVM& ob2);
+	friend int comp(PEVM& ob1, PEVM& ob2);
+	friend ostream& operator << (ostream& out, const PEVM& ob);
+	friend istream& operator >> (istream& in, PEVM& ob);
+
+};
---
+++ LAB 2/LAB 2/LAB 2.cpp
@@ -0,0 +1,108 @@
+﻿#include "Header.h"
+
+
+void PEVM::set_proc(string p) { proc = p; }
+void PEVM::set_mgz(int m) { mgz = m; }
+void PEVM::set_ram(int r) { ram = r; }
+void PEVM::set_type(string t) { type = t; }
+
+string PEVM::get_proc() const { return proc; }
+int PEVM::get_mgz() const { return mgz; }
+int PEVM::get_ram() const { return ram; }
+string PEVM::get_type() const { return type; }
+
+void PEVM::shapka() {
+	printf("|---------------------------------------------|\n");
+	printf("             |Характеристики ПЭВМ|  \n");
+	printf("|---------------------------------------------|\n");
+	printf("|       Процессор      | Частота  | RAM | Тип |\n");
+	printf("|----------------------|----------|-----|-----|\n");
+}
+
+PEVM PEVM::operator = (const PEVM& ob) {
+	proc = ob.proc;
+	mgz = ob.mgz;
+	ram = ob.ram;
+	type = ob.type;
+	return *this;
+}
+PEVM operator + (const PEVM& ob1, const PEVM& ob2) {
+	PEVM temp;
+	temp.proc = ob1.proc + " / " + ob2.proc;
+	temp.mgz = ob1.mgz + ob2.mgz;
+	temp.ram = ob1.ram + ob2.ram;
+	temp.type = ob1.type + " / " + ob2.type;
+	return temp;
+}
+int comp(PEVM& ob1, PEVM& ob2) {
+	return ob1.mgz - ob2.mgz;
+}
+ostream& operator << (ostream& out, const PEVM& ob) {
+	printf("| %21s| %9d| %4d| %4s|\n", ob.proc.c_str(), ob.mgz, ob.ram, ob.type.c_str());
+	return out;
+}
+
+istream& operator >> (istream& in, PEVM& ob) {
+	cout << "Процессор = ";    in >> ob.proc;
+	cout << "Частота (Mgz) = ";    in >> ob.mgz;
+	cout << "RAM (MB) = ";	in >> ob.ram;
+	cout << "Тип = ";	in >> ob.type;
+	cout << endl;
+	return in;
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int q, k, l;
+	PEVM ob[3];
+	string* line = new string [4];
+	ifstream in("D:\\Программы\\CPP_Projects\\2 kurs\\OOP\\LAB 2\\text.txt");
+	if (in.is_open())
+	{	
+		for (int i = 0; i < 3; i++)
+		{
+				for (int j = 0; j < 4; j++)	in >> line[j];
+				ob[i].set_proc(line[0]);
+				q = atoi(line[1].c_str());
+				ob[i].set_mgz(q);
+				q = atoi(line[2].c_str());
+				ob[i].set_ram(q);
+				ob[i].set_type(line[3]);
+				if (in.eof()) break;
+		}
+	}
+	in.close();
+	while (true) {
+		cout << "Выберите:" << endl << "1) Присвоить одному объекту информацию другого;" << endl
+			<< "2) Сложить информацию двух объектов;" << endl
+			<< "3) Проверить объекты на соответствие частоты;" << endl
+			<< "4) Вывести таблицу." << endl;
+		cin >> k;
+		if (k == 1) {
+			cout << "Укажите объект, которому нужно что-то присвоить: "; cin >> k;
+			cout << "Укажите объект, из которого берется информация: "; cin >> l;
+			ob[k] = ob[l];
+		}
+		else if (k == 2) {
+			cout << "Укажите первый объект: "; cin >> k;
+			cout << "Укажите второй объект: "; cin >> l;
+			cout << endl << endl << ob[k] + ob[l] << endl;
+		}
+		else if (k == 3) {
+			cout << "Укажите первый объект: "; cin >> k;
+			cout << "Укажите второй объект: "; cin >> l;
+			if (comp(ob[k], ob[l]) < 0)cout << endl << "Частота первого больше" << endl << endl;
+			else if (comp(ob[k], ob[l]) > 0)cout << endl << "Частота второго больше" << endl << endl;
+			else cout << endl << "Они равны" << endl << endl;
+		}
+		else if (k == 4) {
+			ob[0].shapka();
+			for (int i = 0; i < 3; i++)
+			{
+				cout << ob[i];
+			}
+		}
+	}
+}
